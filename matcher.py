39b489f173bdb3271a6f0dafce40d57a3d58d96d
evhatt <136391335+evhatt@users.noreply.github.com>
2023-06-12 15:22:35 -0700

    Add files via upload
    Made the lists it prints not end with a comma

--- matcher.py
+++ matcher.py
@@ -7,13 +7,18 @@ tempTable = re.compile("(?<=TEMP TABLE )\w+", flags=re.IGNORECASE)
 match2 = list(set(tempTable.findall(report)))
 
 print("Resources Used: ", end="")
-for x in match1:
-        if x not in match2:
-                print(x + ", ", end="")
+for i, x in enumerate(match1):
+        if x in match2:
+                continue
+        if i:
+                print(", ", end='')
+        print(x, end="")
 
 print("\n\nTemp Tables: ", end="")
 if match2 == []:
         print("None")
 else:
-        for x in match2:
-                print(x + ", ", end="")
+        for i, x in enumerate(match2):
+                if i:
+                        print(", ", end="")
+                print(x, end="")
